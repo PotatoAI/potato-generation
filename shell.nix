f730c5e87f19b63a416d602b810ce46b990b7526
Max Gonzih <gonzih@gmail.com>
2022-09-12 21:58:54 -0700

    Maybe this will fix it?

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 with pkgs;
 mkShell {
   nativeBuildInputs = [ pkgconfig ];
-  buildInputs = [ openssl ffmpeg ];
+  buildInputs = [ openssl ffmpeg python39Packages.toml ];
   shellHook = ''
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
       pkgs.lib.makeLibraryPath [ stdenv.cc.cc.lib ]
